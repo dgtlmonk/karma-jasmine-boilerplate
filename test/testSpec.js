624230343197b3ea3e878a870b8130aeeaf6c7e0
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-05-31 02:10:41 +0800

    karma test runner boilerplate

---
+++ test/testSpec.js
@@ -0,0 +1,13 @@
+"use strict";
+
+describe('a test suite', function() {
+    it('should return true', function() {
+        expect(1).toBe(1);
+    });
+
+    it('should fail', function() {
+        expect(false).toBe(true);
+    });
+
+
+});
